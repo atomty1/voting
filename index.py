c8fbbc7c19f1649b2b513340cfc75013f69dbf77
Taye Abidakun <tayeabidakun@gmail.com>
2025-01-28 20:24:13 +0100

    handle contestant function added

--- index.py
+++ index.py
@@ -1,4 +1,12 @@
 def voting_app():
     print("Welcome to our polls")
+    handle_contestants()
+    
 
+def handle_contestants():
+    first_contestant = input("Enter the first contestant name: ")
+    print(f"{first_contestant} added successfully")
+    second_contestant = input("Enter the second contestant name: ")
+    print(f"{second_contestant} added successfully")
+    
 voting_app()
